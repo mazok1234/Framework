0786ae1432b12e00737000e200b163c33755bf73
mazok1234 <andriamahasetra2007@gmail.com>
2026-06-27 20:23:09 +0300

    Map

--- run.bat
+++ run.bat
@@ -1,14 +1,14 @@
 @echo off
 chcp 65001 >nul
-title Framework - Test de compilation
+title Framework - Test de compilation et Deploiement
 
 echo ============================================
-echo   Framework - Test de compilation et JAR
+echo   Framework - Test de compilation et Deploiement
 echo ============================================
 echo.
 
 REM === ETAPE 1: Nettoyage du build ===
-echo [1/4] Nettoyage du repertoire build...
+echo [1/6] Nettoyage du repertoire build...
 if exist "build" (
     rmdir /s /q "build"
 )
@@ -17,7 +17,7 @@ echo      OK
 echo.
 
 REM === ETAPE 2: Compilation des sources ===
-echo [2/4] Compilation des sources Java...
+echo [2/6] Compilation des sources Java...
 dir /s /b "src\*.java" > sources.txt
 javac -cp "lib\*" -d "build" @sources.txt 2> build_errors.txt
 
@@ -39,18 +39,75 @@ echo      Compilation reussie !
 echo.
 
 REM === ETAPE 3: Copie des librairies ===
-echo [3/4] Copie des librairies...
+echo [3/6] Copie des librairies...
 if not exist "build\lib" mkdir "build\lib"
 xcopy "lib\*" "build\lib\" /E /Y >nul
 echo      OK
 echo.
 
 REM === ETAPE 4: Creation du JAR ===
-echo [4/4] Creation du fichier JAR...
+echo [4/6] Creation du fichier JAR...
 cd /d "build"
 jar -cvf framework.jar * >nul
 cd ..
 echo      JAR cree : build\framework.jar
+echo.
+
+REM === ETAPE 5: Creation de l'application de test ===
+echo [5/6] Creation de l'application de test...
+if exist "build\testapp" rmdir /s /q "build\testapp"
+mkdir "build\testapp\WEB-INF\lib"
+mkdir "build\testapp\WEB-INF\classes\controller"
+copy "build\framework.jar" "build\testapp\WEB-INF\lib\" >nul
+xcopy "lib\*" "build\testapp\WEB-INF\lib\" /E /Y >nul
+
+(
+echo package controller;
+echo.
+echo import mg.itu.myframework.annotation.Controller;
+echo import mg.itu.myframework.annotation.UrlMapping;
+echo.
+echo @Controller
+echo public class TestController {
+echo     @UrlMapping^(url="/hello"^)
+echo     public void sayHello^(^) {
+echo     }
+echo }
+) > "build\TestController.java"
+
+javac -cp "build\framework.jar;lib\*" -d "build\testapp\WEB-INF\classes" "build\TestController.java" 2>nul
+del "build\TestController.java" 2>nul
+
+(
+echo ^<?xml version="1.0" encoding="UTF-8"?^>
+echo ^<web-app xmlns="https://jakarta.ee/xml/ns/jakartaee"^>
+echo     ^<servlet^>
+echo         ^<servlet-name^>FrontController^</servlet-name^>
+echo         ^<servlet-class^>mg.itu.myframework.controller.FrontControllerServlet^</servlet-class^>
+echo     ^</servlet^>
+echo     ^<servlet-mapping^>
+echo         ^<servlet-name^>FrontController^</servlet-name^>
+echo         ^<url-pattern^>/*^</url-pattern^>
+echo     ^</servlet-mapping^>
+echo ^</web-app^>
+) > "build\testapp\WEB-INF\web.xml"
+
+echo      Application de test creee dans build\testapp.
+echo.
+
+REM === ETAPE 6: Deploiement sur Tomcat et Lancement ===
+echo [6/6] Deploiement sur Tomcat et lancement...
+set "TOMCAT_WEBAPPS=C:\Tomcat10\apache-tomcat-10.1.24\webapps"
+if exist "%TOMCAT_WEBAPPS%\testapp" rmdir /s /q "%TOMCAT_WEBAPPS%\testapp"
+xcopy "build\testapp" "%TOMCAT_WEBAPPS%\testapp\" /E /I /Y >nul
+
+echo      Demarrage de Tomcat...
+call "C:\Tomcat10\apache-tomcat-10.1.24\bin\startup.bat"
+
+echo      Ouverture du navigateur...
+timeout /t 3 /nobreak >nul
+start http://localhost:8080/testapp/hello
+echo      Termine !
 echo.
 
 echo ============================================
@@ -65,6 +122,8 @@ dir /b "lib\*.jar"
 echo.
 echo JAR genere :
 dir "build\framework.jar"
+echo.
+echo Test de l'application deployee sur : http://localhost:8080/testapp/hello
 echo.
 
 pause
--- src/mg/itu/myframework/util/ClassUtil.java
+++ src/mg/itu/myframework/util/ClassUtil.java
@@ -3,13 +3,18 @@ package mg.itu.myframework.util;
 import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfo;
 import io.github.classgraph.ScanResult;
+import mg.itu.myframework.annotation.UrlMapping;
+import java.lang.reflect.Method;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+
 import java.lang.annotation.Annotation;
 
-public class Util {
+public class ClassUtil {
 
-    public static List<Class<?>> getClassesAnnotation(List<String> packageNames, Class<? extends Annotation> annotation) {
+    public static List<Class<?>> getClassesWithAnnotation(List<String> packageNames, Map<String, MethodClassMapping> urlMappings, Class<? extends Annotation> annotation) {
         List<Class<?>> classes = new ArrayList<>();
         for (String packageName : packageNames) {
             classes.addAll(getClasses(packageName));
@@ -18,12 +23,18 @@ public class Util {
         for (Class<?> clazz : classes) {
             if (clazz.isAnnotationPresent(annotation)) {
                 result.add(clazz);
+                for (Method method : clazz.getDeclaredMethods()) {
+                    if (method.isAnnotationPresent(UrlMapping.class)) {
+                        UrlMapping urlMapping = method.getAnnotation(UrlMapping.class);
+                        String url = urlMapping.url();
+                        MethodClassMapping mapping = new MethodClassMapping(clazz, method);
+                        urlMappings.put(url, mapping);
+                    }
+                }
             }
         }
         return result;
     }
-
-
 
     public static List<Class<?>> getClasses(String packageName) {
         List<Class<?>> classes = new ArrayList<>();
